7ebb7a06132a64ef5477e31ecc25779694aed0e4
iekilinc <iekilinc@proton.me>
2023-02-21 22:59:50 +0300

    Add new routers for posts and post likes

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
 import { createTRPCRouter } from "./trpc";
 import { exampleRouter } from "./routers/example";
+import { postLikeRouter } from "./routers/post-like";
+import { postRouter } from "./routers/post";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +10,8 @@ import { exampleRouter } from "./routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  post: postRouter,
+  postLike: postLikeRouter,
 });
 
 // export type definition of API
