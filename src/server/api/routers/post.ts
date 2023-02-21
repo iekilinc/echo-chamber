32de4dd954b8364e907ec929f2228d6884d9685c
iekilinc <iekilinc@proton.me>
2023-02-21 22:57:18 +0300

    Implement post create, read, and infinite query endpoints

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,22 +1,59 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-import { idSchema, postBodySchema } from "../../../utils/schemas";
+import {
+  idSchema,
+  postBodySchema,
+  postSortSchema,
+} from "../../../utils/schemas";
 
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
 export const postRouter = createTRPCRouter({
-  create: protectedProcedure
+  // https://www.prisma.io/docs/concepts/components/prisma-client/pagination#cursor-based-pagination
+  getScrolling: publicProcedure
     .input(
       z.object({
-        body: postBodySchema,
+        sortingMethod: postSortSchema.default("NEWEST"),
+        cursorPostId: idSchema.optional(),
       })
     )
+    .query(async ({ ctx, input }) => {
+      const { prisma } = ctx;
+      const { sortingMethod, cursorPostId } = input;
+
+      const limit = 10;
+      const isFirstPage: boolean = cursorPostId === undefined;
+
+      const posts = await prisma.post.findMany({
+        take: limit + 1,
+        skip: isFirstPage ? undefined : 1, // skip fetching the post acting as the cursor
+        cursor: { id: cursorPostId },
+        orderBy: {
+          createdAt: sortingMethod === "NEWEST" ? "desc" : "asc",
+        },
+      });
+
+      let nextCursor: typeof cursorPostId;
+      if (posts.length > limit) {
+        const nextPost = posts.pop();
+        nextCursor = nextPost?.id;
+      }
+
+      return {
+        posts,
+        nextCursor,
+      };
+    }),
+
+  create: protectedProcedure
+    .input(z.object({ body: postBodySchema }))
     .mutation(async ({ ctx, input }) => {
       const { prisma } = ctx;
       const { user } = ctx.session;
 
       const post = await prisma.post.create({
         data: {
-          id: user.id,
+          userId: user.id,
           body: input.body,
         },
       });
@@ -35,8 +72,23 @@ export const postRouter = createTRPCRouter({
           id: input.postId,
         },
         select: {
+          id: true, // for error logging
           userId: true,
         },
+      });
+
+      if (user.id !== post.userId && user.role === "USER") {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: `User ${user.id} does not have permissions to delete post ${post.id}`,
+        });
+      }
+
+      await prisma.post.delete({
+        where: {
+          id: post.id,
+        },
+        select: {},
       });
     }),
 });
