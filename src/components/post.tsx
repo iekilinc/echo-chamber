ec4db66c330707c56e10537294a54179804709de
iekilinc <iekilinc@proton.me>
2023-02-27 23:22:21 +0300

    Lay out and style post component

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -3,10 +3,23 @@ import { useState } from "react";
 import { api, type RouterOutputs } from "../utils/api";
 import HeartIconOutline from "@heroicons/react/24/outline/HeartIcon";
 import HeartIconSolid from "@heroicons/react/24/solid/HeartIcon";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
-export const Post: React.FC<
-  RouterOutputs["post"]["getScrolling"]["posts"][number]
-> = ({ id, author, body, likeCount: initialLikeCount, alreadyLiked }) => {
+export const Post: React.FC<{
+  post: RouterOutputs["post"]["getScrolling"]["posts"][number];
+}> = ({
+  post: {
+    id,
+    author,
+    body,
+    createdAt,
+    likeCount: initialLikeCount,
+    alreadyLiked,
+  },
+}) => {
+  const router = useRouter();
+  const { data: session, status } = useSession();
   const [liked, setLiked] = useState(alreadyLiked);
   const [likeCount, setLikeCount] = useState(initialLikeCount);
 
@@ -31,42 +44,54 @@ export const Post: React.FC<
       ? "LIKED"
       : "NOT_LIKED";
 
+  const LikeIcon =
+    likeState === "NOT_LIKED" ? HeartIconOutline : HeartIconSolid;
+
   return (
-    <article>
-      <div>
-        <Image
-          src={author.image ?? ""}
-          alt={`${author.displayName ?? author.username}'s avatar`}
-        />
-        <span>
-          {author.displayName ?? <p>{author.displayName}</p>}
-          <p>@{author.username}</p>
-        </span>
-      </div>
-
-      <main>{body}</main>
-
-      <div>
-        <button
-          onClick={() => {
-            if (likeState === "CHANGING") return;
-            const mutation =
-              likeState === "LIKED" ? unlikeMutation : likeMutation;
-            return mutation.mutateAsync({ postId: id });
-          }}
-        >
-          {(() => {
-            switch (likeState) {
-              case "NOT_LIKED":
-                return <HeartIconOutline />;
-              case "LIKED":
-                return <HeartIconSolid />;
-              case "CHANGING":
-                return <HeartIconSolid className="opacity-50" />;
-            }
-          })()}
-        </button>
-        <span>{likeCount}</span>
+    <article className="flex gap-3 border-y border-neutral-800 bg-[#111] px-3 pt-3">
+      <Image
+        // apple-touch-icon.png is 180x180 px
+        src={author.image ?? "http://localhost:3000/apple-touch-icon.png"}
+        alt={`${author.displayName ?? author.username}'s avatar`}
+        width={96}
+        height={96}
+        className="h-11 w-11 rounded-full"
+      />
+      <div className="flex w-full flex-col">
+        <div className="flex justify-between">
+          <div className="flex gap-1.5">
+            <h3 className="font-bold text-white">
+              {author.displayName ? author.displayName : `@${author.username}`}
+            </h3>
+            {author.displayName && (
+              <span className=" text-[0.95rem] text-neutral-400">
+                @{author.username}
+              </span>
+            )}
+          </div>
+          <div className="place-self-center text-sm text-neutral-400">
+            {createdAt.toLocaleDateString()}
+          </div>
+        </div>
+        <main className="mb-1 leading-snug text-neutral-200">{body}</main>
+        <div className="mb-1 flex gap-0.5">
+          <button
+            onClick={() => {
+              if (!session) {
+                return void router.push("/auth/signin");
+              }
+              if (likeState === "CHANGING") return;
+              const mutation =
+                likeState === "LIKED" ? unlikeMutation : likeMutation;
+              return mutation.mutateAsync({ postId: id });
+            }}
+          >
+            <LikeIcon
+              className={`${likeState === "CHANGING" ? "opacity-40 " : ""}w-4`}
+            />
+          </button>
+          <span className="text-[0.95rem]">{likeCount}</span>
+        </div>
       </div>
     </article>
   );
