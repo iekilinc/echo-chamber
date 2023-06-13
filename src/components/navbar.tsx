71cdfb4f3d2b93fec82a9a44046d212cbfb27e47
iekilinc <iekilinc@proton.me>
2023-06-13 22:43:03 +0300

    Add post button, and implement signout on sidebar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,39 +1,48 @@
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import {
   HomeIcon,
   UserIcon,
   ArrowLeftOnRectangleIcon,
   ArrowRightOnRectangleIcon,
+  PencilSquareIcon,
 } from "@heroicons/react/24/outline";
 
 export const Navbar: React.FC = () => {
   const { data: session } = useSession();
 
   return (
-    <nav className="fixed h-screen w-12 sm:w-28">
-      <ul className="flex h-screen flex-col items-center gap-3 py-4 sm:items-start">
+    <nav className="fixed h-screen w-12 sm:w-32">
+      <ul className="flex h-screen flex-col items-center gap-3 py-6 sm:items-start">
         <NavItem text="Home" href="/" Icon={HomeIcon} />
         <NavItem
           text="Profile"
-          href={session ? "/profile" : "/auth/signin"}
+          href={
+            session ? `/profile/${session.profile.username}` : "/auth/signin"
+          }
           Icon={UserIcon}
         />
-        <div className="mt-auto">
-          {session ? (
+        {session ? (
+          <>
+            <div className="mt-auto">
+              <NavItem text="Post" href="/post" Icon={PencilSquareIcon} />
+            </div>
             <NavItem
               text="Log out"
-              href="/auth/signout"
+              href="/"
               Icon={ArrowRightOnRectangleIcon}
+              onClick={() => void signOut()}
             />
-          ) : (
+          </>
+        ) : (
+          <div className="mt-auto">
             <NavItem
               text="Log in"
               href="/auth/signin"
               Icon={ArrowLeftOnRectangleIcon}
             />
-          )}
-        </div>
+          </div>
+        )}
       </ul>
     </nav>
   );
@@ -45,10 +54,11 @@ const NavItem: React.FC<{
   text: string;
   href: Parameters<typeof Link>[0]["href"];
   Icon: Icon;
-}> = ({ text, href, Icon }) => {
+  onClick?: () => void;
+}> = ({ text, href, Icon, onClick }) => {
   return (
-    <li className="w-[6.5rem] rounded-full border-[1px] border-gray-600 bg-slate-800 p-1 sm:px-2">
-      <Link href={href} className="flex items-center gap-1.5">
+    <li className="rounded-full border-[1px] border-gray-600 bg-slate-800 p-1 sm:w-28 sm:px-2">
+      <Link href={href} className="flex items-center gap-1.5" onClick={onClick}>
         <Icon className="w-7" />
         <h3 className="hidden h-max sm:block">{text}</h3>
       </Link>
