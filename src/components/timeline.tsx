3d1a557cc72a24c30ee744d273b182a6eeab40c8
iekilinc <iekilinc@proton.me>
2023-06-13 12:19:49 +0300

    Remove sign-in button from timeline
    The newly-introduces sidebar includes a sign-in button.

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -16,12 +16,6 @@ const Timeline: React.FC = () => {
 
   return (
     <main className="mx-auto flex max-w-lg flex-col py-4 sm:max-w-2xl">
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={session ? () => void signOut() : () => void signIn()}
-      >
-        {session ? "Sign out" : "Sign in"}
-      </button>
       {postQuery.isLoading && <h1>Loading</h1>}
       {postQuery.error && <h1>{JSON.stringify(postQuery.error)}</h1>}
       {/* {postQuery.data &&
