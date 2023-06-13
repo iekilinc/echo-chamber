6ea99adbb0d8694cf857a8998a9d6eaede744120
iekilinc <iekilinc@proton.me>
2023-06-13 23:32:54 +0300

    Comply with TS import type rule

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -15,8 +15,8 @@
  * These allow you to access things when processing a request, like the
  * database, the session, etc.
  */
-import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
-import { type Session } from "next-auth";
+import type { CreateNextContextOptions } from "@trpc/server/adapters/next";
+import type { Session } from "next-auth";
 
 import { getServerAuthSession } from "../auth";
 import { prisma } from "../db";
