806316ee21776dfff17950c7d771934ed850a283
iekilinc <iekilinc@proton.me>
2023-06-13 22:44:47 +0300

    Transition to Layour in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,9 @@
-import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { Navbar } from "../components/navbar";
-import { api } from "../utils/api";
-import Timeline from "../components/timeline";
+import type { NextPage } from "next";
+import { Layout } from "../components/layout";
+import { Timeline } from "../components/timeline";
 
 const Home: NextPage = () => {
-  return (
-    <div className="bg-gray-900 text-gray-300">
-      <Head>
-        <title>Echo Chamber</title>
-        <meta
-          name="description"
-          content="Echo Chamber, the social media site"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="mx-auto flex h-full min-h-screen w-full justify-center sm:max-w-2xl">
-        <div className="w-12 flex-none sm:w-28">
-          <Navbar />
-        </div>
-        <Timeline />
-      </div>
-    </div>
-  );
+  return <Layout Slot={Timeline} />;
 };
 
 export default Home;
