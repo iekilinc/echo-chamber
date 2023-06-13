1319e0d8ba6be18da78211f1980797df0b8de142
iekilinc <iekilinc@proton.me>
2023-06-13 12:18:16 +0300

    Include navbar in index page and update styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,28 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { Navbar } from "../components/navbar";
 import { api } from "../utils/api";
 import Timeline from "../components/timeline";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className="bg-gray-900 text-gray-300">
       <Head>
         <title>Echo Chamber</title>
-        <meta name="description" content="Social media site" />
+        <meta
+          name="description"
+          content="Echo Chamber, the social media site"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex min-h-screen w-full justify-items-center bg-black text-neutral-100">
+      <div className="mx-auto flex h-full min-h-screen w-full justify-center sm:max-w-2xl">
+        <div className="w-12 flex-none sm:w-28">
+          <Navbar />
+        </div>
         <Timeline />
       </div>
-    </>
+    </div>
   );
 };
 
