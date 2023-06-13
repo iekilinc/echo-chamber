a1a2833a369f28a7ccff7f3c908f8e8f97c48cb9
iekilinc <iekilinc@proton.me>
2023-06-13 22:48:21 +0300

    Make timeline paginated

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,31 +1,37 @@
-import { signIn, signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { api } from "../utils/api";
 import { Post } from "./post";
 
-const Timeline: React.FC = () => {
-  // const postQuery = api.post.getScrolling.useInfiniteQuery(
-  //   {},
-  //   {
-  //     getNextPageParam: (lastPage) => lastPage.nextCursor,
-  //   }
-  // );
+export const Timeline: React.FC<{ authorId?: string }> = ({ authorId }) => {
   const { data: session } = useSession();
-  const postQuery = api.post.getMany.useQuery();
-
   console.log({ session });
 
+  const postQuery = api.post.getScrolling.useInfiniteQuery(
+    { authorId },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+  const posts = postQuery.data?.pages.flatMap((page) => page.posts);
+
+  postQuery.hasNextPage;
+
   return (
-    <main className="flex flex-col gap-[1px] bg-gray-600">
-      {postQuery.isLoading && <h1>Loading</h1>}
-      {postQuery.error && <h1>{JSON.stringify(postQuery.error)}</h1>}
-      {/* {postQuery.data &&
-        postQuery.data.pages.flatMap(({ posts }) =>
-          posts.map((post) => <Post post={post} key={post.id} />)
-        )} */}
-      {postQuery.data &&
-        postQuery.data.map((post) => <Post post={post} key={post.id} />)}
-    </main>
+    <div className="flex w-full flex-col items-center gap-5">
+      <main className="flex flex-col gap-[1px] bg-gray-600 p-[1px] w-full">
+        {postQuery.isLoading && <h1>Loading</h1>}
+        {postQuery.error && <h1>{JSON.stringify(postQuery.error)}</h1>}
+        {posts && posts.map((post) => <Post post={post} key={post.id} />)}
+      </main>
+      {postQuery.hasNextPage && (
+        <button
+          onClick={() => void postQuery.fetchNextPage()}
+          className="rounded-full border-gray-600 bg-slate-800 py-2 px-4"
+          disabled={postQuery.isFetchingNextPage}
+        >
+          Load more
+        </button>
+      )}
+    </div>
   );
 };
-
-export default Timeline;
