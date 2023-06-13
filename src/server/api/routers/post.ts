c00ee4c0fd7b880a797c03771ef0afce49319002
iekilinc <iekilinc@proton.me>
2023-06-13 12:16:13 +0300

    Create endpoint for getting multiple posts

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -73,6 +73,83 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
+  getMany: publicProcedure.query(async ({ ctx }) => {
+    const { prisma, session } = ctx;
+
+    const dbUniqueAuthorPosts = await prisma.profile.findMany({
+      take: 30,
+      orderBy: { createdAt: "desc" },
+      select: {
+        posts: {
+          take: 1,
+          orderBy: { createdAt: "desc" },
+          select: {
+            id: true,
+            author: {
+              select: {
+                displayName: true,
+                username: true,
+                user: { select: { image: true } },
+              },
+            },
+            createdAt: true,
+            body: true,
+            // To check if the logged-in user has already liked the post
+            postLikes: !session
+              ? false
+              : {
+                  where: { likerId: session.profile.id },
+                  select: { id: true },
+                },
+            _count: { select: { postLikes: true } },
+          },
+        },
+      },
+    });
+
+    // const dbPosts = await prisma.post.findMany({
+    //   take: 30,
+    //   orderBy: { createdAt: "desc" },
+    //   select: {
+    //     id: true,
+    //     author: {
+    //       select: {
+    //         displayName: true,
+    //         username: true,
+    //         user: { select: { image: true } },
+    //       },
+    //     },
+    //     body: true,
+    //     // To check if the logged-in user has already liked the post
+    //     postLikes: !session
+    //       ? undefined
+    //       : { where: { likerId: session.profile.id } },
+    //     _count: { select: { postLikes: true } },
+    //   },
+    // });
+
+    // const posts = dbUniqueAuthorPosts.map((p) => {
+    const posts = dbUniqueAuthorPosts.flatMap((author) =>
+      author.posts.map((p) => ({
+        id: p.id,
+        body: p.body,
+        author: {
+          displayName: p.author.displayName,
+          username: p.author.username,
+          image: p.author.user.image,
+        },
+        createdAt: p.createdAt,
+        likeCount: p._count.postLikes,
+        // postLikes were filtered by session.profile.id; therefore, postLikes
+        // are only the likes belonging to the current user (if there is) one
+        alreadyLiked: p.postLikes && p.postLikes.length > 0,
+      }))
+    );
+    // }));
+
+    return posts;
+  }),
+
   create: protectedProcedure
     .input(z.object({ body: postBodySchema }))
     .mutation(async ({ ctx, input }) => {
